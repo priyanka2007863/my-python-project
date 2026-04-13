60385f9bd7dec448ecb8a1c692c863e3dbff51ce
Priyanka Chaurasiya <priyankachaurasiya6509@gmail.com>
2026-04-13 13:38:52 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,26 @@
+# arithmetic operators
+
+x= 10
+y= 5
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
+
+#comparison operators
+print(x==y)   #false
+print(x<y)    #false
+print(x>y)    #true
+
+# logical operators
+print(x>y and x<y)   #false
+print(x>y or x<y)    #true
+print(not(x>y))      #false
+
+#Assignment operator
+a= 2
+b= 3
+a= a+6
+a+=6
